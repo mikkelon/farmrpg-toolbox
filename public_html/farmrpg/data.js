b89bf69a4319575c47c3c8927602ccc84befe4e4
Mikkel Overgaard <mikkel@onielsen.dk>
2022-03-06 17:16:56 +0100

    wishing well search tool

---
+++ public_html/farmrpg/data.js
@@ -0,0 +1,253 @@
+const wwData = [{
+    input: 'Aquamarine',
+    outputs: ['Amethyst', 'Emerald', 'Lemon Quartz', 'Ruby']
+},{
+    input: 'Old Boot',
+    outputs: ['4-leaf Clover', 'Glass Bottle', 'Ancient Coin']
+},{
+    input: 'Shark Tooth',
+    outputs: ['4-leaf Clover', 'Moonstone', 'Bacon']
+},{
+    input: 'Jade',
+    outputs: ['Amethyst', 'Aquamarine', 'Emberstone', 'Ruby']
+},{
+    input: 'Taffy',
+    outputs: ['Ancient Coin', 'Lollipop', 'Candy']
+},{
+    input: 'Pine Cone',
+    outputs: ['Antler', 'Horn', 'Prism Shard']
+},{
+    input: 'Candy',
+    outputs: ['Apple', 'Lollipop', 'Spider', 'Taffy']
+},{
+    input: 'Antler',
+    outputs: ['Arrowhead', 'Bird Egg', 'Pine Cone']
+},{
+    input: 'Dice',
+    outputs: ['Bacon', 'Small Flute', 'Spectacles', 'Teapot']
+},{
+    input: 'Gold Cucumber',
+    outputs: ['Gold Carrot', 'Gold Eggplant', 'Bacon']
+},{
+    input: 'Gold Eggplant',
+    outputs: ['Bacon', 'Gold Carrot', 'Gold Leaf', 'Gold Peas']
+},{
+    input: 'Gold Feather',
+    outputs: ['Bacon', 'Gold Leaf', 'Runestone 05']
+},{
+    input: 'Gold Leaf',
+    outputs: ['Bacon', 'Gold Feather', 'Magicite', 'Runestone 07']
+},{
+    input: 'Salt',
+    outputs: ['Broccoli', 'Spiked Shell']
+},{
+    input: 'Chattering Teeth',
+    outputs: ['Candy', 'Lollipop', 'Taffy', 'Treat Bag 01', 'Witch Hat']
+},{
+    input: 'Lollipop',
+    outputs: ['Candy', 'Spider', 'Taffy']
+},{
+    input: 'Gouda',
+    outputs: ['Candy Corn','Chattering Teeth', 'Wax Candle']
+},{
+    input: 'Jack-o-lantern',
+    outputs: ['Candy', 'Egyptian Necklace', 'Goldfish', 'Spider']
+},{
+    input: 'R.O.A.S.',
+    outputs: ['Candy Corn', 'Lollipop', 'Square Key', 'Taffy']
+},{
+    input: 'Treat Bag 03',
+    outputs: ['Candy Corn', 'Goldfish', 'Jack-o-lantern', 'Moonstone']
+},{
+    input: 'Ancient Coin',
+    outputs: ['Captains Log', 'Cogwheel', 'Eye Patch', 'Freaky Picture', 'Giant Centipede', 'Octopus', 'Orange Gecko', 'Shiny Beetle', 'Small Gear', 'Small Screw', 'Snail', 'Spectacles', 'Wax Candle']
+},{
+    input: 'Pirate Bandana',
+    outputs: ['Captains Log', 'Carved Bear', 'Eye Patch', 'Pirate Flag', 'Spectacles']
+},{
+    input: 'Pirate Flag',
+    outputs: ['Sealed Letter', 'Small Flute', 'Small Gear', 'Spectacles']
+},{
+    input: 'Teapot',
+    outputs: ['Pirate Flag', 'Carved Owl', 'Carved Rhino', 'Carved Warthog', 'Caterpillar', 'Freaky Picture', 'Runestone 02', 'Runestone 03']
+},{
+    input: 'Small Flute',
+    outputs: ['Captains Log', 'Carved Warthog', 'Dice', 'Small Screw', 'Strange Letter']
+},{
+    input: 'Wooden Mask',
+    outputs: ['Carved Owl', 'Small Flute', 'Spectacles']
+},{
+    input: 'Spectacles',
+    outputs: ['Carved Squirrel', 'Dice', 'Pirate Bandana', 'Small Screw', 'Wooden Pipe']
+},{
+    input: 'Fire Ant',
+    outputs: ['Caterpillar', 'Giant Centipede', 'Horned Beetle', 'Runestone 08']
+},{
+    input: 'Shiny Beetle',
+    outputs: ['Caterpillar', 'Horned Beetle', 'Runestone 05', 'Snail']
+},{
+    input: 'Eyepatch',
+    outputs: ['Spectacles', 'Moonstone', 'Runestone 01', 'Runestone 02']
+},{
+    input: 'Snail',
+    outputs: ['Caterpillar', 'Runestone 10', 'Shiny Beetle', 'Horned Beetle']
+},{
+    input: 'Witch Hat',
+    outputs: ['Chattering Teeth', 'Wax Candle', 'Wizard Hat']
+},{
+    input: 'Essence of Slime',
+    outputs: ['Chum', 'Wooden Bow']
+},{
+    input: 'Pocket Watch',
+    outputs: ['Cogwheel', 'Pirate Bandana', 'Wooden Mask']
+},{
+    input: 'Herbs',
+    outputs: ['Corn', 'Flamejack', 'Gold Sea Bass', 'Green Jellyfish', 'Honey', 'Octopus']
+},{
+    input: 'Frog',
+    outputs: ['Crab Claw', 'Orange Gecko', 'Sea Dragon']
+},{
+    input: 'Orange Gecko',
+    outputs: ['Crab Claw', 'Frog', 'Sea Dragon']
+},{
+    input: 'Sea Dragon',
+    outputs: ['Crab Claw', 'Fancy Drum', 'Frog', 'Orange Gecko']
+},{
+    input: 'Strange Letter',
+    outputs: ['Dice', 'Freaky Picture', 'Small Screw', 'Spectacles']
+},{
+    input: 'Wooden Pipe',
+    outputs: ['Dice', 'Fancy Pipe', 'Small Flute', 'Small Gear']
+},{
+    input: 'Candy Corn',
+    outputs: ['Egyptian Necklace', 'Lollipop', 'R.O.A.S.', 'Taffy']
+},{
+    input: 'Treat Bag 02',
+    outputs: ['Taffy', 'Egyptian Necklace', 'Square Key']
+},{
+    input: 'Captains Log',
+    outputs: ['Eye Patch', 'Pirate Flag', 'Strange Letter']
+},{
+    input: 'Small Gear',
+    outputs: ['Fancy Box', 'Gouda', 'Wax Candle']
+},{
+    input: 'Caterpillar',
+    outputs: ['Fire Ant', 'Giant Centipede', 'Runestone 09', 'Shiny Beetle']
+},{
+    input: 'Horned Beetle',
+    outputs: ['Fire Ant', 'Giant Centipede', 'Runestone 06', 'Shiny Beetle']
+},{
+    input: 'Crab Claw',
+    outputs: ['Frog', 'Orange Gecko', 'Sea Dragon']
+},{
+    input: 'Gold Peppers',
+    outputs: ['Gold Carrot', 'Gold Cucumber', 'Gold Eggplant', 'Gold Peas', 'Runestone 01']
+},{
+    input: 'Gold Drum',
+    outputs: ['Gold Catfish', 'Gold Trout', 'Sturdy Bow']
+},{
+    input: 'Gold Flier',
+    outputs: ['Gold Catfish', 'Gold Sea Bass', 'Goldgill']
+},{
+    input: 'Goldfin',
+    outputs: ['Gold Catfish', 'Gold Trout']
+},{
+    input: 'Goldgill',
+    outputs: ['Gold Catfish', 'Goldfin', 'Goldjack']
+},{
+    input: 'Gold Sea Bass',
+    outputs: ['Gold Coral', 'Gold Flier', 'Goldfin']
+},{
+    input: 'Gold Carrot',
+    outputs: ['Gold Cucumber', 'Gold Eggplant', 'Gold Peas', 'Strange Letter']
+},{
+    input: 'Gold Peas',
+    outputs: ['Gold Cucumber', 'Gold Eggplant', 'Gold Feather', 'Gold Peppers']
+},{
+    input: 'Bacon',
+    outputs: ['Gold Feather', 'Gold Leaf', 'Magicite']
+},{
+    input: 'Runestone 04',
+    outputs: ['Gold Feather', 'Runestone 03', 'Gold Leaf']
+},{
+    input: 'Runestone 06',
+    outputs: ['Gold Feather', 'Gold Leaf', 'Runestone 07', 'Runestone 10']
+},{
+    input: 'Runestone 08',
+    outputs: ['Gold Feather', 'Runestone 09', 'Runestone 10']
+},{
+    input: 'Runestone 09',
+    outputs: ['Gold Feather', 'Runestone 08', 'Runestone 10']
+},{
+    input: 'Magicite',
+    outputs: ['Gold Leaf', 'Runestone 01', 'Runestone 02', 'Strange Letter']
+},{
+    input: 'Runestone 07',
+    outputs: ['Gold Leaf', 'Runestone 08', 'Runestone 10']
+},{
+    input: 'Goldjack',
+    outputs: ['Gold Sea Bass', 'Goldgill']
+},{
+    input: 'Gold Coral',
+    outputs: ['Goldfin', 'Sturdy Bow']
+},{
+    input: 'Gold Catfish',
+    outputs: ['Goldgill', 'Goldjack', 'Wooden Bow']
+},{
+    input: 'Gold Trout',
+    outputs: ['Gold Flier', 'Goldgill', 'Goldray']
+},{
+    input: 'Wax Candle',
+    outputs: ['Gouda', 'R.O.A.S.', 'Small Gear', 'Witch Hat']
+},{
+    input: 'Water Lily',
+    outputs: ['Grab Bag 02', 'Mushroom Paste', 'Popcorn']
+},{
+    input: 'Giant Centipede',
+    outputs: ['Horned Beetle', 'Runestone 07', 'Shiny Beetle', 'Snail']
+},{
+    input: 'Apple Cider',
+    outputs: ['Iced Tea', 'Lemonade', 'Orange Juice']
+},{
+    input: 'Runestone 02',
+    outputs: ['Magicite', 'Magna Quartz', 'Runestone 01', 'Runestone 03']
+},{
+    input: 'Runestone 05',
+    outputs: ['Magicite', 'Magna Quartz', 'Runestone 04', 'Runestone 06']
+},{
+    input: '4-leaf Clover',
+    outputs: ['MIAB', 'Small Flute', 'Wooden Mask']
+},{
+    input: 'Eye Patch',
+    outputs: ['Moonstone', 'Runestone 01', 'Runestone 02', 'Spectacles']
+},{
+    input: 'Runestone 01',
+    outputs: ['Moonstone', 'Runestone 02', 'Strange Letter']
+},{
+    input: 'Runestone 03',
+    outputs: ['Moonstone', 'Runestone 04', 'Runestone 05']
+},{
+    input: 'Freaky Picture',
+    outputs: ['Pirate Flag', 'Small Gear', 'Small Screw', 'Small Spring', 'Teapot']
+},{
+    input: 'Cogwheel',
+    outputs: ['Pocket Watch', 'Small Flute', 'Small Screw', 'Wooden Mask']
+},{
+    input: 'Moonstone',
+    outputs: ['Runestone 01', 'Runestone 02', 'Runestone 07', 'Strange Letter']
+},{
+    input: 'Runestone 10',
+    outputs: ['Runestone 06', 'Runestone 08', 'Runestone 09']
+},{
+    input: 'Large Chest 01',
+    outputs: ['Small Key', 'Square Key', 'Treasure Key']
+},{
+    input: 'Large Chest 02',
+    outputs: ['Small Key', 'Square Key', 'Treasure Key']
+},{
+    input: 'Small Spring',
+    outputs: ['Strange Letter', 'Spectacles', 'Small Screw', 'Wooden Pipe']
+},{
+    input: '',
+    outputs: ['']
+}]
--- public_html/farmrpg/main.js
+++ public_html/farmrpg/main.js
@@ -1,20 +1,94 @@
-const btnPlus = document.getElementById('button-plus');
-const btnMinus = document.getElementById('button-minus');
-const counttxt = document.getElementById('counter');
-let count = 0;
+//Wishing Well Search Tool
+const wwButton = document.getElementById('ww-button');
+const wwText = document.getElementById('ww-text');
+const userInputBox = document.getElementById('ww-user-input');
+const wwList = document.getElementById('ww-list');
+const wwOutput = document.getElementById('ww-output');
 
-function updateCount() {
-    counttxt.innerHTML = count;
-}
+//Function for creating list item in HTML document
+function createListItem(string, list) {
+    let li = document.createElement('li');
+    li.innerHTML = string;
+    list.appendChild(li);
+};
 
-updateCount();
+//Function for searching through output array
+function wwSearchOutputs(output) {
+    return wwData.some((el1) => {
+        return el1.outputs.some((el2) => {
+            return el2.toLowerCase() === output.toLowerCase();
+        });
+    });
+};
 
-btnPlus.addEventListener("click", () => {
-    count++;
-    updateCount();
-});
+//Function for finding inputs
+function elementHasOutput(array, string) {
+    return array.some((element) => {
+        return element.toLowerCase() === string.toLowerCase();
+    });
+};
 
-btnMinus.addEventListener("click", () => {
-    count--;
-    updateCount();
-});
+function findPercentage(value) {
+    let count = 0;
+    wwData.forEach((element) => {
+        if (element.input.toLowerCase() === value.toLowerCase()) {
+            count++;
+        };
+    });
+    let percentage = Math.floor(1 / count * 100);
+    return percentage;
+};
+
+function findInput() {
+    wwList.innerHTML = '';
+    const userInput = userInputBox.value;
+    //if-statement checks if userInput is found in any of the outputs arrays
+    if (wwSearchOutputs(userInput) === true) {
+        let inputsArray = [];
+        wwData.forEach((element) => { //iterates through all elements in wwData
+            if (elementHasOutput(element.outputs, userInput) === true) { //if-statement checks if element has what the user is looking for
+                let arr = [];
+                const percentage = Math.floor(1 / element.outputs.length * 100);
+                arr.push(element.input); //pushes input for said output to an array inputsArray
+                arr.push(percentage);
+                inputsArray.push(arr);
+            };
+        });
+        inputsArray.sort((a,b) => {return b[1] - a[1]});
+        inputsArray.forEach((element) => { //itereates through all elements in inputsArray
+            createListItem(`<strong>${element[0]}</strong> with a <strong>${element[1]}%</strong> drop chance.`, wwList); //Creates a list item for each element in inputsArray
+        });
+    } else {
+        createListItem(`No outputs were found for '<strong>${userInput}</strong>'.`, wwList);
+    };
+};
+
+/* function findInput() {
+    wwOutput.innerHTML = userInputBox.value;
+    wwList.innerHTML = '';
+    const userInput = userInputBox.value.toLowerCase();
+    let outputArr = [];
+    wwData.forEach((element) => {
+        if (element.output.toLowerCase() === userInput) {
+            let percentage = 0;
+            wwData.forEach((element2) => {
+                if (element.input === element2.input) {
+                    percentage++;
+                };
+            });
+            percentage = Math.floor(1 / percentage * 100);
+            let li = document.createElement('li');
+            li.innerHTML = `<strong>${element.input}</strong> with a ${percentage}% chance to drop.`;
+            wwList.appendChild(li);
+        };
+    });
+}; */
+
+//Event Listeners//
+userInputBox.addEventListener('keydown', (event) => { //Event Listeners for input-box 'Enter' key.
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        findInput();
+    }
+})
+wwButton.addEventListener('click', findInput); // Event listener for click on search button
